9c80177da2fb7993c29db3a38bb384d763175b72
Михаил Марин <mmn-83@yandex.ru>
2025-11-01 12:22:07 +0300

    create main script

--- main.py
+++ main.py
@@ -1,59 +1,83 @@
-import argparse
 import csv
 
+from argparse import ArgumentParser
 from tabulate import tabulate
 
 
-FILES_DIR = 'test_files'
+REPORT_CHOISES = {
+    'brand-rating-average': ['brand', 'rating'],
+    'brand-price-average': ['brand', 'price'],
+    'name-rating-average': ['name', 'rating'],
+    'name-price-average': ['name', 'price'],
+}
 
 
-def get_parser_args(parser) -> tuple:
+def get_parser_args(parser: ArgumentParser) -> tuple:
     """Функция получения аргументов командной строки."""
 
     parser.add_argument(
+        '--report',
+        choices=list(REPORT_CHOISES),
+        default='brand-rating-average',
+        type=str,
+        help='Наименование отчета'
+    )
+    parser.add_argument(
         '--files',
+        type=str,
         nargs="+",
         help='Выберите файлы'
     )
     parser.add_argument(
-        '--report',
-        default='average-rating',
+        '--dir-path',
+        default='./files/',
         type=str,
-        help='Наименование отчета'
+        help='Директория с файлами'
     )
 
-    files_data = parser.parse_args().files
     report_name = parser.parse_args().report
+    files_data = parser.parse_args().files
+    dir_path = parser.parse_args().dir_path
 
-    return files_data, report_name
+    return report_name, files_data, dir_path
 
 
-def get_report_data(files_data: list, files_dir: str) -> dict:
-    """Функция парсинга файлов csv."""
-    
+def create_report_data(
+    files_data: list,
+    dir_path: str,
+    position_row: str,
+    culculating_row: str
+) -> tuple[list, str, str]:
+    """Функция парсинга файлов csv и формирования сводного отчета."""
+
     elem_report = {}
 
-    for elem in files_data:
+    for current_file in files_data:
 
-        with open(f'./{files_dir}/{elem}', mode='r') as file:
+        with open(f'{dir_path}{current_file}', mode='r') as file:
             csv_reader = csv.DictReader(file)
             for row in csv_reader:
-                elem_report.setdefault(row['brand'], []).append(float(row['rating']))
+                elem_report.setdefault(row[position_row], []).append(float(row[culculating_row]))
 
     report_data = [[key, round(sum(values_list) / len(values_list), 2)] for key, values_list in elem_report.items()]
     report_data.sort(key=lambda i: i[1], reverse=True)
 
-    return report_data
+    return report_data, position_row, culculating_row
 
 
-def print_report_table(report_name: str, report_data: dict):
-    """Функция консольного вывода таблицы с результатами."""
+def print_report_table(
+    report_name: str,
+    report_data: dict,
+    position_row: str,
+    culculating_row: str
+):
+    """Функция консольного вывода отчетной таблицы."""
 
     table = tabulate(
         report_data,
-        headers = ['brand', 'rating'],
+        headers = [position_row, culculating_row],
         tablefmt='outline',
-        showindex='always'
+        showindex=(indx for indx, _ in enumerate(report_data, start=1))
     )
 
     print(report_name, table, sep='\n')
@@ -61,13 +85,17 @@ def print_report_table(report_name: str, report_data: dict):
 
 def main():
 
-    parser = argparse.ArgumentParser(description="Average Rating")
+    parser = ArgumentParser(description='Report')
 
-    files_data, report_name = get_parser_args(parser)
-    report_data = get_report_data(files_data, FILES_DIR)
-    report_table = print_report_table(report_name, report_data)
+    report_name, files_data, dir_path = get_parser_args(parser)
 
-    return report_table
+    report_data, position_row, culculating_row = create_report_data(
+        files_data,
+        dir_path,
+        *REPORT_CHOISES[report_name]
+    )
+
+    print_report_table(report_name, report_data, position_row, culculating_row)
 
 
 if __name__ == '__main__':
